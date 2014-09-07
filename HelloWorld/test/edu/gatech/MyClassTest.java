2cf2b6e9e69795cfd9ef204bee4e045de55ddcc7
Sven Roepke <sven.roepke@gatech.edu>
2014-09-07 19:02:43 -0400

    updated class

--- HelloWorld/test/edu/gatech/MyClassTest.java
+++ HelloWorld/test/edu/gatech/MyClassTest.java
@@ -2,13 +2,21 @@ package edu.gatech;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyClassTest {
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testExceptionIsThrown() {
+    MyClass tester = new MyClass();
+    tester.multiply(1000, 5);
+  }
 
-	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
-	}
-
-}
+  @Test
+  public void testMultiply() {
+    MyClass tester = new MyClass();
+    assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+  }
+} 
