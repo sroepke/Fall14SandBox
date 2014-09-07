95727d6ff1afae471ae70231dc0f4ca9ac2dfd68
Sven Roepke <sven.roepke@gatech.edu>
2014-09-07 19:00:28 -0400

    added JUnit test cast

---
+++ HelloWorld/test/edu/gatech/MyClassTest.java
@@ -0,0 +1,14 @@
+package edu.gatech;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+}
